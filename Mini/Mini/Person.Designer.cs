68d6253a314438519a29ae7d5503c24302cc5ef0
AlizaFarrukh <43961393+AlizaFarrukh@users.noreply.github.com>
2019-03-12 20:54:25 +0500

    first commit

---
+++ Mini/Mini/Person.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace Mini
+{
+    partial class Person
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.idT = new System.Windows.Forms.TextBox();
+            this.id = new System.Windows.Forms.Label();
+            this.firstName = new System.Windows.Forms.Label();
+            this.lastName = new System.Windows.Forms.Label();
+            this.contact = new System.Windows.Forms.Label();
+            this.email = new System.Windows.Forms.Label();
+            this.dob = new System.Windows.Forms.Label();
+            this.gender = new System.Windows.Forms.Label();
+            this.firstNameT = new System.Windows.Forms.TextBox();
+            this.LastNameT = new System.Windows.Forms.TextBox();
+            this.contactT = new System.Windows.Forms.TextBox();
+            this.emailT = new System.Windows.Forms.TextBox();
+            this.dobT = new System.Windows.Forms.TextBox();
+            this.genderT = new System.Windows.Forms.ComboBox();
+            this.person = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.addB = new System.Windows.Forms.Button();
+            this.deleteB = new System.Windows.Forms.Button();
+            this.updateB = new System.Windows.Forms.Button();
+            this.dataL = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // idT
+            // 
+            this.idT.Location = new System.Drawing.Point(174, 64);
+            this.idT.Name = "idT";
+            this.idT.Size = new System.Drawing.Size(100, 20);
+            this.idT.TabIndex = 0;
+            // 
+            // id
+            // 
+            this.id.AutoSize = true;
+            this.id.Location = new System.Drawing.Point(48, 71);
+            this.id.Name = "id";
+            this.id.Size = new System.Drawing.Size(18, 13);
+            this.id.TabIndex = 1;
+            this.id.Text = "ID";
+            this.id.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // firstName
+            // 
+            this.firstName.AutoSize = true;
+            this.firstName.Location = new System.Drawing.Point(48, 112);
+            this.firstName.Name = "firstName";
+            this.firstName.Size = new System.Drawing.Size(57, 13);
+            this.firstName.TabIndex = 2;
+            this.firstName.Text = "First Name";
+            // 
+            // lastName
+            // 
+            this.lastName.AutoSize = true;
+            this.lastName.Location = new System.Drawing.Point(48, 153);
+            this.lastName.Name = "lastName";
+            this.lastName.Size = new System.Drawing.Size(58, 13);
+            this.lastName.TabIndex = 3;
+            this.lastName.Text = "Last Name";
+            // 
+            // contact
+            // 
+            this.contact.AutoSize = true;
+            this.contact.Location = new System.Drawing.Point(48, 199);
+            this.contact.Name = "contact";
+            this.contact.Size = new System.Drawing.Size(44, 13);
+            this.contact.TabIndex = 4;
+            this.contact.Text = "Contact";
+            // 
+            // email
+            // 
+            this.email.AutoSize = true;
+            this.email.Location = new System.Drawing.Point(48, 239);
+            this.email.Name = "email";
+            this.email.Size = new System.Drawing.Size(32, 13);
+            this.email.TabIndex = 5;
+            this.email.Text = "Email";
+            // 
+            // dob
+            // 
+            this.dob.AutoSize = true;
+            this.dob.Location = new System.Drawing.Point(48, 278);
+            this.dob.Name = "dob";
+            this.dob.Size = new System.Drawing.Size(66, 13);
+            this.dob.TabIndex = 6;
+            this.dob.Text = "Date of Birth";
+            this.dob.Click += new System.EventHandler(this.dob_Click);
+            // 
+            // gender
+            // 
+            this.gender.AutoSize = true;
+            this.gender.Location = new System.Drawing.Point(48, 315);
+            this.gender.Name = "gender";
+            this.gender.Size = new System.Drawing.Size(42, 13);
+            this.gender.TabIndex = 7;
+            this.gender.Text = "Gender";
+            this.gender.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // firstNameT
+            // 
+            this.firstNameT.Location = new System.Drawing.Point(174, 105);
+            this.firstNameT.Name = "firstNameT";
+            this.firstNameT.Size = new System.Drawing.Size(100, 20);
+            this.firstNameT.TabIndex = 8;
+            // 
+            // LastNameT
+            // 
+            this.LastNameT.Location = new System.Drawing.Point(174, 150);
+            this.LastNameT.Name = "LastNameT";
+            this.LastNameT.Size = new System.Drawing.Size(100, 20);
+            this.LastNameT.TabIndex = 9;
+            // 
+            // contactT
+            // 
+            this.contactT.Location = new System.Drawing.Point(174, 192);
+            this.contactT.Name = "contactT";
+            this.contactT.Size = new System.Drawing.Size(100, 20);
+            this.contactT.TabIndex = 10;
+            // 
+            // emailT
+            // 
+            this.emailT.Location = new System.Drawing.Point(174, 236);
+            this.emailT.Name = "emailT";
+            this.emailT.Size = new System.Drawing.Size(100, 20);
+            this.emailT.TabIndex = 11;
+            // 
+            // dobT
+            // 
+            this.dobT.Location = new System.Drawing.Point(174, 275);
+            this.dobT.Name = "dobT";
+            this.dobT.Size = new System.Drawing.Size(100, 20);
+            this.dobT.TabIndex = 12;
+            // 
+            // genderT
+            // 
+            this.genderT.FormattingEnabled = true;
+            this.genderT.Items.AddRange(new object[] {
+            "Male",
+            "Female"});
+            this.genderT.Location = new System.Drawing.Point(174, 312);
+            this.genderT.Name = "genderT";
+            this.genderT.Size = new System.Drawing.Size(100, 21);
+            this.genderT.TabIndex = 14;
+            this.genderT.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // person
+            // 
+            this.person.AutoSize = true;
+            this.person.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.person.Location = new System.Drawing.Point(106, 9);
+            this.person.Name = "person";
+            this.person.Size = new System.Drawing.Size(100, 31);
+            this.person.TabIndex = 15;
+            this.person.Text = "Person";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(300, 71);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(744, 150);
+            this.dataGridView1.TabIndex = 16;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick_2);
+            // 
+            // addB
+            // 
+            this.addB.Location = new System.Drawing.Point(199, 360);
+            this.addB.Name = "addB";
+            this.addB.Size = new System.Drawing.Size(75, 23);
+            this.addB.TabIndex = 17;
+            this.addB.Text = "ADD";
+            this.addB.UseVisualStyleBackColor = true;
+            this.addB.Click += new System.EventHandler(this.addB_Click);
+            // 
+            // deleteB
+            // 
+            this.deleteB.Location = new System.Drawing.Point(580, 268);
+            this.deleteB.Name = "deleteB";
+            this.deleteB.Size = new System.Drawing.Size(75, 23);
+            this.deleteB.TabIndex = 18;
+            this.deleteB.Text = "Delete";
+            this.deleteB.UseVisualStyleBackColor = true;
+            this.deleteB.Click += new System.EventHandler(this.deleteB_Click);
+            // 
+            // updateB
+            // 
+            this.updateB.Location = new System.Drawing.Point(694, 268);
+            this.updateB.Name = "updateB";
+            this.updateB.Size = new System.Drawing.Size(75, 23);
+            this.updateB.TabIndex = 20;
+            this.updateB.Text = "Update";
+            this.updateB.UseVisualStyleBackColor = true;
+            this.updateB.Click += new System.EventHandler(this.updateB_Click);
+            // 
+            // dataL
+            // 
+            this.dataL.AutoSize = true;
+            this.dataL.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dataL.Location = new System.Drawing.Point(482, 44);
+            this.dataL.Name = "dataL";
+            this.dataL.Size = new System.Drawing.Size(52, 24);
+            this.dataL.TabIndex = 21;
+            this.dataL.Text = "Data:";
+            this.dataL.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(927, 494);
+            this.Controls.Add(this.dataL);
+            this.Controls.Add(this.updateB);
+            this.Controls.Add(this.deleteB);
+            this.Controls.Add(this.addB);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.person);
+            this.Controls.Add(this.genderT);
+            this.Controls.Add(this.dobT);
+            this.Controls.Add(this.emailT);
+            this.Controls.Add(this.contactT);
+            this.Controls.Add(this.LastNameT);
+            this.Controls.Add(this.firstNameT);
+            this.Controls.Add(this.gender);
+            this.Controls.Add(this.dob);
+            this.Controls.Add(this.email);
+            this.Controls.Add(this.contact);
+            this.Controls.Add(this.lastName);
+            this.Controls.Add(this.firstName);
+            this.Controls.Add(this.id);
+            this.Controls.Add(this.idT);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox idT;
+        private System.Windows.Forms.Label id;
+        private System.Windows.Forms.Label firstName;
+        private System.Windows.Forms.Label lastName;
+        private System.Windows.Forms.Label contact;
+        private System.Windows.Forms.Label email;
+        private System.Windows.Forms.Label dob;
+        private System.Windows.Forms.Label gender;
+        private System.Windows.Forms.TextBox firstNameT;
+        private System.Windows.Forms.TextBox LastNameT;
+        private System.Windows.Forms.TextBox contactT;
+        private System.Windows.Forms.TextBox emailT;
+        private System.Windows.Forms.TextBox dobT;
+        private System.Windows.Forms.ComboBox genderT;
+        private System.Windows.Forms.Label person;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button addB;
+        private System.Windows.Forms.Button deleteB;
+        private System.Windows.Forms.Button updateB;
+        private System.Windows.Forms.Label dataL;
+    }
+}
+
---
+++ Mini/Mini/Person.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Mini
+{
+    public partial class Person : Form
+    {
+        SqlConnection conn = new SqlConnection(@"Data Source=HAIER-PC\SQLEXPRESS;Initial Catalog=ProjectA;Integrated Security=True");
+        SqlCommand cmd;
+        SqlDataAdapter prog;
+        int Id = 0;
+        public Person()
+        {
+            InitializeComponent();
+        }
+        private void DisplayData()
+        {
+            conn.Open();
+            DataTable dt = new DataTable();
+            prog = new SqlDataAdapter("select * from Person", conn);
+            prog.Fill(dt);
+            dataGridView1.DataSource = dt;
+            conn.Close();
+        }
+        private void ClearData()
+        {
+            firstNameT.Text = "";
+            LastNameT.Text = "";
+            contactT.Text = "";
+            emailT.Text = "";
+            dobT.Text = "";
+            Id = 0;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void gendercombo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+
+        private void addB_Click(object sender, EventArgs e)
+        {
+            if (firstNameT.Text != "" && LastNameT.Text != "" && contactT.Text != "" && emailT.Text != "" && dobT.Text != "")
+            {
+                cmd = new SqlCommand("insert into Person(FirstName,LastName,Contact,Email,DateOfBirth) values(@FirstName,@LastName,@Contact,@Email,@DateOfBirth)", conn);
+                conn.Open();
+                cmd.Parameters.AddWithValue("@FirstName", firstNameT.Text);
+                cmd.Parameters.AddWithValue("@LastName", LastNameT.Text);
+                cmd.Parameters.AddWithValue("@Contact", contactT.Text);
+                cmd.Parameters.AddWithValue("@Email", emailT.Text);
+                cmd.Parameters.AddWithValue("@DateOfBirth", dobT.Text);
+                cmd.ExecuteNonQuery();
+                conn.Close();
+                MessageBox.Show("Data Inserted Successfully");
+                DisplayData();
+                ClearData();
+
+            }
+            else
+            {
+                MessageBox.Show("Please Enter Details!");
+            }
+        }
+
+        private void updateB_Click(object sender, EventArgs e)
+        {
+            if (firstNameT.Text != "" && LastNameT.Text != "" && contactT.Text != "" && emailT.Text != "" && dobT.Text != "")
+            {
+                cmd = new SqlCommand("update Person set FirstName=@firstName,LastName=@lastName, Contact=@Contact, Email=@email, DateOfBirth=@dob where Id=@id", conn);
+                conn.Open();
+                cmd.Parameters.AddWithValue("@id", Id);
+                cmd.Parameters.AddWithValue("@firstName", firstNameT.Text);
+                cmd.Parameters.AddWithValue("@lastName", LastNameT.Text);
+                cmd.Parameters.AddWithValue("@Contact", contactT.Text);
+                cmd.Parameters.AddWithValue("@email", emailT.Text);
+                cmd.Parameters.AddWithValue("@dob", dobT.Text);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Data Updated Successfully");
+                conn.Close();
+                DisplayData();
+                ClearData();
+            }
+            else
+            {
+                MessageBox.Show("Please Select Data to Update");
+            }
+        }
+
+        private void FN_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DisplayData();
+        }
+        private void dob_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+
+            
+        }
+
+        private void deleteB_Click(object sender, EventArgs e)
+        {
+            if (Id != 0)
+            {
+                cmd = new SqlCommand("delete Person where ID=@id", conn);
+                conn.Open();
+                cmd.Parameters.AddWithValue("@id", Id);
+                cmd.ExecuteNonQuery();
+                conn.Close();
+                MessageBox.Show("Data Deleted Successfully!");
+                DisplayData();
+                ClearData();
+            }
+            else
+            {
+                MessageBox.Show("Please Select Data to Delete");
+            }
+        }
+
+        private void dataGridView1_CellContentClick_2(object sender, DataGridViewCellEventArgs e)
+        {
+            Id = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString());
+            firstNameT.Text = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+            LastNameT.Text = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+            contactT.Text = dataGridView1.Rows[e.RowIndex].Cells[3].Value.ToString();
+            emailT.Text = dataGridView1.Rows[e.RowIndex].Cells[4].Value.ToString();
+            dobT.Text = dataGridView1.Rows[e.RowIndex].Cells[5].Value.ToString();
+        }
+    }
+}
